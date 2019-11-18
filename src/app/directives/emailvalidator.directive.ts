418c192e3ca088549fa02970c9ed2c24977c0954
SAMM <muroy.sergio@gmail.com>
2019-11-18 12:54:37 -0500

    Directive correction

--- src/app/directives/emailvalidator.directive.ts
+++ src/app/directives/emailvalidator.directive.ts
@@ -1,12 +1,12 @@
 import { Directive } from '@angular/core';
 // import {NG_ASYNC_VALIDATORS} from '@angular/common';
 //
-// @Directive({
-//   selector: '[emailValidator]',
-//   providers:  provide(NG_ASYNC_VALIDATORS, {
-//       useExisting: EmailValidator, multi: true
-//     }),
-// })
+@Directive({
+  selector: '[emailValidator]',
+  // providers:  provide(NG_ASYNC_VALIDATORS, {
+  //     useExisting: EmailValidator, multi: true
+  //   }),
+})
 export class EmailvalidatorDirective {
 
   constructor() { }
